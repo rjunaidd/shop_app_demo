7154e832ab792186cf013798249c264f9aea4215
Rauf Junaid <roafjunaid09@gmail.com>
2026-05-23 06:00:46 +0500

    update code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,64 +10,77 @@ class HomePage extends StatelessWidget {
     "Nike",
     "Ndure",
     "Bata",
-    "Jordan"
+    "Jordan",
   ];
 
   @override
   Widget build(BuildContext context) {
-
     final border = OutlineInputBorder(
-        borderSide: BorderSide(
-          color: Color.fromRGBO(225, 225, 225, 1),
-        ),
-        borderRadius: BorderRadius.horizontal(
-            left: Radius.circular(50)
-        )
+      borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
     );
-    return  SafeArea(
-        child: Scaffold(
-          body: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text("Shoe\nCollection" ,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Lato',
-                          fontSize: 20
-                      ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "Shoe\nCollection",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Lato',
+                      fontSize: 20,
                     ),
                   ),
-                  Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Search",
-                          hintStyle: TextStyle(),
-                          prefixIcon: Icon(Icons.search ),
-                          border: border,
-                          enabledBorder: border,
-                          focusedBorder: border
-                        ),
-                      )
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  itemCount:  filters.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder:(context , index){
-                    final filter = filters[index];
-                    return Chip(label: Text(filter));
-                  },
                 ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(),
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 80,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Chip(
+                      backgroundColor: Color.fromRGBO(245, 247, 249, 1),
+                        label: Text(filter),
+                    labelStyle: TextStyle(
+                      fontSize: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    padding:  EdgeInsets.symmetric(
+                      horizontal: 18,
+                      vertical: 10
+                    ),
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
-    ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
