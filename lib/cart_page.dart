9614c0932447db6770ecb8878b6bdcd3d9618dca
Rauf Junaid <roafjunaid09@gmail.com>
2026-05-25 06:45:47 +0500

    cart screen

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app_demo/global_variables.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,10 +11,32 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Cart Page"),
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Cart"),
+        centerTitle: true,
       ),
+      body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context , index){
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(onPressed: (){
+
+            }, icon: Icon(Icons.delete,
+            color: Colors.red,)
+            ),
+            title: Text(cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            subtitle: Text("Size: ${cartItem['size']}"),
+          );
+          }
+          ),
     );
   }
 }
--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -34,14 +34,14 @@ final products= [
 ];
 
 final cart =  [
-  {
+
     {
       'id' : '1',
       "title" : 'Jordan Shoes',
       "price" : 35.5,
       "imageUrl" : "assets/images/shoes_2.png",
       'company' : 'Jordan Shoes',
-      "sizes" : [9, 10, 11, 12],
+      "sizes" : 9,
     }
-  }
+
 ];
